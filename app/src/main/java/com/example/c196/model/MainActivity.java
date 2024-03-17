ab30d4aa76196168400afa8332b5c1e18c389cac
Neelofar <nkarim1@wgu.edu>
2024-03-16 21:47:20 -0400

    changes for new computer

--- app/src/main/java/com/example/c196/model/MainActivity.java
+++ app/src/main/java/com/example/c196/model/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.c196.model;
 
+import static android.app.ProgressDialog.show;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.c196.R;
 
@@ -12,5 +18,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    //Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu,menu);
+        return true;
+    }
+
+    //Menu item selection
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.action_home) {
+            Toast.makeText(this, "You selected Home", Toast.LENGTH_SHORT).show();
+        } else if (itemId == R.id.action_search) {
+            Toast.makeText(this, "You selected Search", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
