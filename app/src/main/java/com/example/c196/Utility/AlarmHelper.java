6a9db13fe2e9c8e73f38df5890507687cd33a541
Neelofar <nkarim1@wgu.edu>
2024-04-19 19:18:10 -0400

    fixed notification feature. second submission

--- app/src/main/java/com/example/c196/Utility/AlarmHelper.java
+++ app/src/main/java/com/example/c196/Utility/AlarmHelper.java
@@ -4,6 +4,8 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.provider.Settings;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -21,6 +23,10 @@ public class AlarmHelper {
         try {
             Calendar date = Calendar.getInstance();
             date.setTime(sdf.parse(dateStr));
+            date.set(Calendar.HOUR_OF_DAY, 0);
+            date.set(Calendar.MINUTE, 0);
+            date.set(Calendar.SECOND, 0);
+            date.set(Calendar.MILLISECOND, 0);
             long trigger = date.getTimeInMillis();
 
             Intent intent = new Intent(context, Alarm.class);
@@ -31,11 +37,22 @@ public class AlarmHelper {
                     context, notificationId, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
 
             AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-            alarmManager.set(AlarmManager.RTC_WAKEUP, trigger, sender);
-
-            Log.d("AlarmHelper", "Alarm set for: " + title + " at " + sdf.format(date.getTime()) + " with ID " + notificationId);
-            Toast.makeText(context, "Notification set for: " + title + " at " + sdf.format(date.getTime()), Toast.LENGTH_SHORT).show();
-
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                if (alarmManager.canScheduleExactAlarms()) {
+                    alarmManager.setExact(AlarmManager.RTC_WAKEUP, trigger, sender);
+                    Log.d("AlarmHelper", "Exact Alarm set for: " + title + " at " + sdf.format(date.getTime()) + " with ID " + notificationId);
+                    Toast.makeText(context, "Exact Notification set for: " + title + " at " + sdf.format(date.getTime()), Toast.LENGTH_SHORT).show();
+                } else {
+                    //get user permission via system settings
+                    Intent permitIntent = new Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM);
+                    context.startActivity(permitIntent);
+                }
+            } else {
+                //if user permission is not needed
+                alarmManager.setExact(AlarmManager.RTC_WAKEUP, trigger, sender);
+                Log.d("AlarmHelper", "Exact Alarm set for older OS: " + title + " at " + sdf.format(date.getTime()) + " with ID " + notificationId);
+                Toast.makeText(context, "Notification set for older OS: " + title + " at " + sdf.format(date.getTime()), Toast.LENGTH_SHORT).show();
+            }
         } catch (Exception e) {
             e.printStackTrace();
             Log.e("AlarmHelper", "Failed to parse date or set notification", e);
